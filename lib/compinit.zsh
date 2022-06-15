92267afad2cffadc03371548a1f4ca07114305ea
Kambiz Tavabi <ktavabi@gmail.com>
2022-06-15 12:22:20 -0700

    fix broken link in custom

--- lib/compinit.zsh
+++ lib/compinit.zsh
@@ -3,10 +3,12 @@ export BASE_SHELL=$(basename $SHELL)
 
 # NB this only works if compinit is loaded prior to changes in $fpath
 # https://apple.stackexchange.com/questions/296477/my-command-line-says-complete13-command-not-found-compdef/340718#340718
+# ln -s compinit.zsh /custom/compinit.zsh
+
 if [[ "$BASE_SHELL" == "zsh" ]] ; then
-autoload -Uz compinit
-compinit -D
-autoload bashcompinit && bashcompinit -D
+    autoload -Uz compinit
+    compinit -D
+    autoload bashcompinit && bashcompinit -D
 fi
 
 _pipenv-pipes_completions() {
